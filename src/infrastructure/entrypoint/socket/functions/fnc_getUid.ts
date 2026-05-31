9f1ca827abf6fe21abe5df05900f0bea6112558a
Lenin Ospina <o.lamprea99@gmail.com>
2026-05-31 17:29:53 -0500

    retirando ws, es incompatible con vercel

--- src/infrastructure/entrypoint/socket/functions/fnc_getUid.ts
+++
@@ -1,3 +0,0 @@
-export default function getUid(id: string, users: { [uuid: string]: string}){
-    return Object.keys(users).find((uid) => users[uid] === id);
-}   
--- src/infrastructure/entrypoint/socket/functions/fnc_sendMessage.ts
+++
@@ -1,6 +0,0 @@
-export default function sendMessage(name: string, users: string[],io: any ,  payload?: Object) {
-    users.forEach( id => payload ?
-            io.to(id).emit(name, payload) :
-            io.to(id).emit(name)
-    )
-}
